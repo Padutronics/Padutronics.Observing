eff9eed9694852cda10e20e9ab07794e86c6dfd1
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-04-28 20:52:12 +0200

    Add observable value holder

---
+++ Source/Padutronics.Observing/ObservableValueHolder.cs
@@ -0,0 +1,49 @@
+using Padutronics.Diagnostics.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Padutronics.Observing;
+
+[DebuggerDisplay(DebuggerDisplayValues.DebuggerDisplay)]
+public sealed class ObservableValueHolder<T> : IObservable<T>
+    where T : IEquatable<T>
+{
+    private T? value;
+
+    public ObservableValueHolder() :
+        this(default)
+    {
+    }
+
+    public ObservableValueHolder(T? value)
+    {
+        Value = value;
+    }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay => $"{Value}";
+
+    public T? Value
+    {
+        get => value;
+        set
+        {
+            if (!EqualityComparer<T>.Default.Equals(this.value, value))
+            {
+                var e = new ValueChangedEventArgs<T?>(oldValue: this.value, newValue: value);
+
+                this.value = value;
+
+                OnValueChanged(e);
+            }
+        }
+    }
+
+    public event EventHandler<ValueChangedEventArgs<T?>>? ValueChanged;
+
+    private void OnValueChanged(ValueChangedEventArgs<T?> e)
+    {
+        ValueChanged?.Invoke(this, e);
+    }
+}
